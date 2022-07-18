ce4ffe56c741b7919d4609429b57a493860f7fd9
uletin <dokterkoding111>
2022-07-18 14:24:34 +0800

    belajar react optimization performance

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useCallback, useMemo } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({ name: "john" })
+  const likeAction = () => setCount(current => current + 1)
+  const memoizedlikeAction = useCallback(likeAction, [])
+
+  const heavyProcess = (user) => {
+    Sleep(2000)
+    return user
+  }
+
+  const userProp = useMemo(() => heavyProcess(user), [user])
+
+  console.log("Parent Components Rendered")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>
+        <i>-- Parent Components --</i>
+      </p>
+      <button onClick={likeAction}>
+        Like {count}{" "}
+      </button>
+      <MemoizedChildComponent
+        title="Hello"
+        user={userProp}
+        action={memoizedlikeAction} />
+    </>
   );
+}
+
+function ChildComponent({ title, user, action }) {
+  // Sleep(2000)
+  console.log("Child Components Rendered")
+  return (
+    <>
+      <p>
+        <br />
+        <i>-- Child Components --</i>
+      </p>
+      <h1>{title} {user.name}</h1>
+      <button onClick={action}> Like </button>
+    </>
+  )
+}
+
+function compare(prevProps, nextProps) {
+  return JSON.stringify(prevProps) === JSON.stringify(nextProps)
+}
+
+const MemoizedChildComponent = React.memo(ChildComponent)
+
+function Sleep(milliseconds) {
+  const date = Date.now()
+  let currentDate = null
+  do {
+    currentDate = Date.now()
+  } while (currentDate - date < milliseconds)
+
 }
 
 export default App;
